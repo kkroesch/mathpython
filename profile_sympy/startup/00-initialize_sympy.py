0df4ba7949b3f1c2a6e7ea6faf619ded7c743ddb
Karsten Kroesch <kkroesch@me.com>
2018-06-25 15:18:49 +0200

    Initial import

---
+++ profile_sympy/startup/00-initialize_sympy.py
@@ -0,0 +1,13 @@
+from __future__ import division
+from sympy import *
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+init_printing()
+import matplotlib.pyplot as plotter
+print("Matplotlib is available via plotter.show().")
+print("""
+x, y, z, t = symbols('x y z t')
+k, m, n = symbols('k m n', integer=True)
+f, g, h = symbols('f g h', cls=Function)
+""")
